cc22861a55eb2ff8f3caf281d7b6cedac361e0b8
h4tice <haticekrc772@gmail.com>
2023-04-13 21:57:38 +0300

    voiceRecorder

---
+++ voiceRecorder/main.py
@@ -0,0 +1,10 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+freq = 44100
+duration=5
+recording=sd.rec(int(duration*freq),samplerate=freq,channels=2)
+sd.wait()
+write("recording1.wav",freq,recording)
+
+
